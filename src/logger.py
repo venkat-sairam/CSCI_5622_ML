916dd6e14d1303e7e7be44d31d90b442e2cfb56d
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2024-07-04 14:43:47 +0530

    added logger module

---
+++ src/logger.py
@@ -0,0 +1,28 @@
+import logging
+import os
+
+from from_root import from_root
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%d-%m-%y_%H-%M-%S')}.log"
+
+log_dir = "logs"
+
+logs_path = os.path.join(from_root(), log_dir, LOG_FILE)
+
+os.makedirs(log_dir, exist_ok=True)
+
+logging.basicConfig(
+    filename=logs_path,
+    format="[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+# Create a console handler
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(
+    logging.Formatter("[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s")
+)
+
+# Add the console handler to the root logger
+logging.getLogger().addHandler(console_handler)
