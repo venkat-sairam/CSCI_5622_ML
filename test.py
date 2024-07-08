fbf0d4aedcdf0377c3b8fac3fb2d4daa7664f81b
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2024-07-09 01:42:18 +0530

    test script

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 from src.pipeline.train_pipeline import *
 
+
 class TestPipeline:
     def __init__(self) -> None:
         pass
-    
+
     def run_pipeline(self):
         obj = Pipeline()
         self.data_ingestion_artifact = obj.start_data_ingestion()
         self.data_transformtion_artifact = obj.start_data_transformations()
+        self.model_trainer_artifact = obj.initiate_model_training()
 
-obj  = TestPipeline()
+
+obj = TestPipeline()
 obj.run_pipeline()
