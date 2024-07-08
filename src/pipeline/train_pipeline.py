5855161e9b72e2323c7603577f313139a6438f7f
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2024-07-09 01:41:32 +0530

    model trainer pipeline

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -1,8 +1,10 @@
 from src.components.data_ingestion import Data_Ingestion
 from src.components.data_transformation import Data_Transformation
+from src.components.model_trainer import ModelTrainer
 from src.logger import logging
 from src.exception import CustomException
 from src.entity.artifact_entity import DataIngestionArtifact, DataTransformtionArtifact
+
 
 def print_before_execution(func):
     def wrapper(*args, **kwargs):
@@ -13,20 +15,26 @@ def print_before_execution(func):
 
     return wrapper
 
+
 class Pipeline:
     def __init__(self):
         print("Inside Pipeline....")
 
-    @print_before_execution        
+    @print_before_execution
     def start_data_ingestion(self):
         obj = Data_Ingestion()
         self.data_ingestion_artifact = obj._initiate_data_ingestion()
         return self.data_ingestion_artifact
-    
+
     @print_before_execution
     def start_data_transformations(self):
-        data_transformaton = Data_Transformation(data_ingestion_artifact= self.data_ingestion_artifact)
-        self.data_transformtion_config = data_transformaton.initiate_data_transformation()
-        return self.data_transformtion_config
+        data_transformaton = Data_Transformation(
+            data_ingestion_artifact=self.data_ingestion_artifact
+        )
+        self.data_transformtion_artifact = data_transformaton.initiate_data_transformation()
+        return self.data_transformtion_artifact
 
-        
+    @print_before_execution
+    def initiate_model_training(self):
+        model_trainer = ModelTrainer(data_transformation_artifact=self.data_transformtion_artifact)
+        model_trainer.initiate_model_trainer()
