1a8f84c6444c34886f997ec228a540c52160b41a
Venkata Sairam Yanamandra <venkatasairam.y@gmail.com>
2024-07-26 22:58:53 +0530

    updated model evaluation component

--- src/components/model_evaluator.py
+++ src/components/model_evaluator.py
@@ -10,6 +10,7 @@ from src.entity.artifact_entity import (
 from src.utils import sys, read_from_csv_file, load_object, DataFrame
 from imblearn.under_sampling import RandomUnderSampler
 
+
 class ModelEvaluator(object):
 
     def __init__(
@@ -50,7 +51,7 @@ class ModelEvaluator(object):
                 self.get_input_and_target_features_from_a_dataframe(
                     df=test_data,
                     columns_to_drop=["depression", "gender", "participant_id"],
-                    target_feature="depression",  # Changed to string as single value
+                    target_feature=["depression"],
                 )
             )
             logging.info(
@@ -65,8 +66,11 @@ class ModelEvaluator(object):
             )
 
             logging.info("removed SMOTE on the testing dataset")
-            X_test_resampled, y_test_resampled = preprocessed_object.transform(test_input_features),test_target_feature,
-
+            X_test_resampled = preprocessed_object["preprocessor"].fit_transform(
+                test_input_features
+            )
+            # X_test_resampled, y_test_resampled = preprocessed_object.transform(test_input_features),test_target_feature,
+            y_test_resampled = test_target_feature
             logging.info("Adding column names to the resampled X_test")
             X_test_resampled_with_cols = DataFrame(
                 X_test_resampled, columns=test_input_features.columns
@@ -109,43 +113,67 @@ class ModelEvaluator(object):
                 }
             )
 
-            participants_results_grouped = participants_results.groupby('participant_id').agg({
-                'true_label': 'first',  # Assumes that the 'true_label' is the same for all turns of a participant
-                'predicted_prob': 'mean',  # Average prediction probability per participant
-                'gender': 'first'  # Gender is the same for all turns of a participant
-            })
+            participants_results_grouped = participants_results.groupby("participant_id").agg(
+                {
+                    "true_label": "first",  # Assumes that the 'true_label' is the same for all turns of a participant
+                    "predicted_prob": "mean",  # Average prediction probability per participant
+                    "gender": "first",  # Gender is the same for all turns of a participant
+                }
+            )
 
             # Threshold the predictions at 0.5 for classification
-            participants_results_grouped['final_prediction'] = (participants_results_grouped['predicted_prob'] >= 0.5).astype(int)
+            participants_results_grouped["final_prediction"] = (
+                participants_results_grouped["predicted_prob"] >= 0.5
+            ).astype(int)
 
             # Calculate the metrics
-            accuracy_A = accuracy_score(participants_results_grouped['true_label'], participants_results_grouped['final_prediction'])
-            balanced_accuracy_BA = balanced_accuracy_score(participants_results_grouped['true_label'], participants_results_grouped['final_prediction'])
+            accuracy_A = accuracy_score(
+                participants_results_grouped["true_label"],
+                participants_results_grouped["final_prediction"],
+            )
+            balanced_accuracy_BA = balanced_accuracy_score(
+                participants_results_grouped["true_label"],
+                participants_results_grouped["final_prediction"],
+            )
 
             # Separate the data for female and male participants
-            female_data = participants_results_grouped[participants_results_grouped['gender'] == 0]
-            male_data = participants_results_grouped[participants_results_grouped['gender'] == 1]
+            female_data = participants_results_grouped[participants_results_grouped["gender"] == 0]
+            male_data = participants_results_grouped[participants_results_grouped["gender"] == 1]
 
             # Calculate the metrics for female and male participants
-            accuracy_female = accuracy_score(female_data['true_label'], female_data['final_prediction'])
-            balanced_accuracy_female = balanced_accuracy_score(female_data['true_label'], female_data['final_prediction'])
-            accuracy_male = accuracy_score(male_data['true_label'], male_data['final_prediction'])
-            balanced_accuracy_male = balanced_accuracy_score(male_data['true_label'], male_data['final_prediction'])
+            accuracy_female = accuracy_score(
+                female_data["true_label"], female_data["final_prediction"]
+            )
+            balanced_accuracy_female = balanced_accuracy_score(
+                female_data["true_label"], female_data["final_prediction"]
+            )
+            accuracy_male = accuracy_score(male_data["true_label"], male_data["final_prediction"])
+            balanced_accuracy_male = balanced_accuracy_score(
+                male_data["true_label"], male_data["final_prediction"]
+            )
 
             # Calculate Equality of Opportunity (EO)
-            conf_matrix_female = confusion_matrix(female_data['true_label'], female_data['final_prediction'])
-            conf_matrix_male = confusion_matrix(male_data['true_label'], male_data['final_prediction'])
-            tpr_female = conf_matrix_female[1, 1] / (conf_matrix_female[1, :].sum() if conf_matrix_female[1, :].sum() != 0 else 1)
-            tpr_male = conf_matrix_male[1, 1] / (conf_matrix_male[1, :].sum() if conf_matrix_male[1, :].sum() != 0 else 1)
+            conf_matrix_female = confusion_matrix(
+                female_data["true_label"], female_data["final_prediction"]
+            )
+            conf_matrix_male = confusion_matrix(
+                male_data["true_label"], male_data["final_prediction"]
+            )
+            tpr_female = conf_matrix_female[1, 1] / (
+                conf_matrix_female[1, :].sum() if conf_matrix_female[1, :].sum() != 0 else 1
+            )
+            tpr_male = conf_matrix_male[1, 1] / (
+                conf_matrix_male[1, :].sum() if conf_matrix_male[1, :].sum() != 0 else 1
+            )
             EO = 1 - abs(tpr_male - tpr_female)
 
             # Print the metrics
-            print(f'Overall Accuracy (A): {accuracy_A}')
-            print(f'Overall Balanced Accuracy (BA): {balanced_accuracy_BA}')
-            print(f'Female Accuracy: {accuracy_female}')
-            print(f'Female Balanced Accuracy: {balanced_accuracy_female}')
-            print(f'Male Accuracy: {accuracy_male}')
-            print(f'Male Balanced Accuracy: {balanced_accuracy_male}')
-            print(f'Equality of Opportunity (EO): {EO}')
+            print(f"Overall Accuracy (A): {accuracy_A}")
+            print(f"Overall Balanced Accuracy (BA): {balanced_accuracy_BA}")
+            print(f"Female Accuracy: {accuracy_female}")
+            print(f"Female Balanced Accuracy: {balanced_accuracy_female}")
+            print(f"Male Accuracy: {accuracy_male}")
+            print(f"Male Balanced Accuracy: {balanced_accuracy_male}")
+            print(f"Equality of Opportunity (EO): {EO}")
         except Exception as e:
             raise CustomException(e)
